b51f84a2aa0a4270c02286e87bd8922f57e25115
espadalko <espadalko@me.com>
2019-11-29 16:10:36 +0700

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <Test1 size="12" level="3" step="35" height="20" />
-    <Test1 height="20" />
-    <Test1 size="16" level="12" step="35" height="20"/>
-  </div>
+	<div id="app">
+		<!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+		<!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+		<div class="slider">
+			<Test1 width="30px" class="test1" /> 
+		</div>
+<!--     <Test1 size="12" level="3" step="35" height="20" />
+	 <Test1 height="20" />
+	 <Test1 size="16" level="12" step="35" height="20"/>
+	-->    
+</div>
+
 </template>
 
 <script>
@@ -13,24 +18,27 @@
 import Test1 from './components/Test1.vue'
 
 export default {
-  name: 'app',
-  components: {
-    // HelloWorld,
-    Test1
-  }
+	name: 'app',
+	components: {
+	 // HelloWorld,
+	 Test1
+	}
 }
 </script>
 
 <style lang="stylus">
 *
-  box-sizing border-box
+	box-sizing border-box
 body 
-  background-color #333
+	background-color #333
 #app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  color #2c3e50
-.indicator
-  height 30px
+	font-family 'Avenir', Helvetica, Arial, sans-serif
+	-webkit-font-smoothing antialiased
+	-moz-osx-font-smoothing grayscale
+	color #2c3e50
+
+.slider
+	width 700px
+	background-color red
+
 </style>
--- src/components/Test1.vue
+++ src/components/Test1.vue
@@ -1,20 +1,20 @@
 <template>
-	<div class="indicator" :style="{width: size * step + 'px', height: height + 'px'}">
- 		<div class="indicator__level" :style="{width: step + 'px', height: height + 'px'}" v-for="n in emptyArray(size)"></div>
-	</div>
+	<div>
+		<div class="slider" :style="{width: width, height: height}">
+			<div class="value"></div>
+		</div>
+ 	</div>
 </template>
 
 <script>
 	export default {
 		props:{
-			size:{type: Number, default: 16},
-			level:{type: Number, default: 8},
-			step:{type: Number, default: 35},
-			height:{type: Number, default: 100}
-		},
-		data(){
-			return {
-			}
+			min: 		{type: Number, default: 0},
+			max: 		{type: Number, default: 100},
+			step: 	{type: Number, default: 1},
+			value: 	{type: Number, default: 0},
+			width: 	{type: String, default: '300px'},
+			height: 	{type: String, default: '30px'}
 		},
 		methods:{
 			emptyArray(num){
@@ -26,18 +26,12 @@
 			}
 		}
 	}
-
 </script>
 
-<style lang="stylus">
-	color = #70A1CF 
-	.indicator
-		display flex
-		margin 10px
-		&__level
-			background-color color
-			border 2px solid transparent 
-			margin-right 4px
-			border-radius 5px
-			// margin 2px
+<style lang="stylus" scoped>
+	// color = #70A1CF 
+	.slider
+		// width 100%
+		// height 20px
+		background-color #fff
 </style>
